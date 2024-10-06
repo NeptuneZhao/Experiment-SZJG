0ee0077711f4565d6b57d5d7c33287122521e711
赵辰瑞 <qq1836143240@outlook.com>
2024-10-06 15:35:06 +0800

    add 2004!!

--- 2004/2004.c
+++ 2004/2004.c
@@ -2,68 +2,99 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define PASS "unpassed"
+#define PASS "passed"
+
+// 我也来玩点神秘的
+#define __DEEP_LENGTH__ 16
 
 typedef struct BinaryTreeNode
 {
     int index;
-    struct BinaryTreeNode *lchild, *rchild, *parent;
+    struct BinaryTreeNode *lchild, *rchild;
 } BiTree;
 
-BiTree* createNode(int index, BiTree* parent)
+BiTree* createNode(int index)
 {
     BiTree* newNode = (BiTree*) malloc(sizeof(BiTree));
 
     newNode -> index = index;
     newNode -> lchild = newNode -> rchild = NULL;
-    newNode -> parent = parent;
     return newNode;
 }
 
-BiTree* createTree(int* nodes, int index, int size, BiTree* parent)
+BiTree* createTree(int* nodes, int index, int size)
 {
     if (index >= size || nodes[index] == -1)
         return NULL;
     
-    BiTree* root = createNode(nodes[index], parent);
+    BiTree* root = createNode(nodes[index]);
 
-    root -> lchild = createTree(nodes, 2 * index + 1, size, root);
-    root -> rchild = createTree(nodes, 2 * index + 2, size, root);
+    root -> lchild = createTree(nodes, 2 * index + 1, size);
+    root -> rchild = createTree(nodes, 2 * index + 2, size);
     return root;
 }
 
-// 第一步, 找到 index 对应的节点
-// All right
-BiTree* findNode(BiTree* node, int index)
+int deepFS(BiTree *node, int end, char *path, int path_len)
 {
-    if (node == NULL)
-        return NULL;
-    else if (node -> index == index)
-        return node;
+    // Nodes is NULL, return 0
+    // It means we don't find
+    if (!node)
+        return 0;
     
-    BiTree* left = findNode(node -> lchild, index);
-    BiTree* right = findNode(node -> rchild, index);
-    return left == NULL ? right : left;
-}
+    path_len += sprintf(path + path_len, "%d->", node -> index);
 
-// 第二步, 从初始节点开始遍历, 直到结束节点(如果找不到直接拜拜)
-char* findChildNode(BiTree* beginNode, BiTree* endNode, char* path)
-{
-
-    if (beginNode == NULL)
-        return "Too Far!!!";
-    else if (beginNode == endNode)
+    // If we find the end node, we return 1, and cut the end of the path "->"
+    if (node -> index == end)
     {
-        return ";";
+        path[path_len - 2] = '\0';
+        return 1;
     }
 
-    findChildNode(beginNode -> lchild, endNode, path);
-    findChildNode(beginNode -> rchild, endNode, path);
+    // If the node is not the end node, we continue to find children
+    if ( deepFS(node -> lchild, end, path, path_len) || deepFS(node -> rchild, end, path, path_len) )
+        return 1;
+    
+    return 0;
+}
+
+void findPath(BiTree *root, int start, int end)
+{
+    BiTree *stack[__DEEP_LENGTH__], *cur = root;
+    int top = -1;
+
+    while (cur || top != -1)
+    {
+        while (cur)
+        {
+            // Don't forget the difference between ++top and top++
+            // ++top means top = top + 1, top++ means top = top and top = top + 1
+            stack[++top] = cur;
+            if (cur -> index == start)
+                break;
+            cur = cur -> lchild;
+        }
+        if (top != -1 && stack[top] -> index == start)
+            break;
+        cur = stack[top--] -> rchild;
+    }
+
+    if (!cur)
+    {
+        printf("Too Far!!!\n");
+        return;
+    }
+
+    char path[__DEEP_LENGTH__ * 4] = {0};
+
+    if (deepFS(cur, end, path, 0))
+        printf("%s\n", path);
+    else
+        printf("Too Far!!!\n");
 }
 
 int main()
 {
-    int n, a, b;
+    int n, start, end;
     scanf("%d", &n);
     
     int* nodes = (int*) calloc(n, sizeof(int));
@@ -76,11 +107,9 @@ int main()
     }
     
     BiTree* root = createTree(nodes, 0, n, NULL);
-    char* num = (char*) malloc(2 * sizeof(char));
-    scanf("%d%d", &a, &b);
-    itoa(a, num, 10);
-    BiTree *beginNode = findNode(root, a), *endNode = findNode(root, b);
-    printf("%s\n", findChildNode(beginNode, endNode, num));
+
+    scanf("%d%d", &start, &end);
+    findPath(root, start, end);
 
     free(nodes);
     free(root);
